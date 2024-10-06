d0220dd1f3bf4907f446d44139f3f7faea0711f4
Soumya <soumyasen1809@gmail.com>
2024-10-06 18:45:58 +0200

    Initial support for TUI
    - Using ratatui for TUI
    - Spawn tokio task to run TUI in a seperate thread

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::io::{self, stdout};
+
+use ratatui::{
+    backend::CrosstermBackend,
+    crossterm::{
+        event::{self, Event, KeyCode},
+        terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+        ExecutableCommand,
+    },
+    widgets::{Block, Paragraph},
+    Frame, Terminal,
+};
 use simple_sys_info::{
     cpu::cpu_usage_meas, disk::disk_utility_meas, memory::memory_consumption_meas,
     socket::net_socket_read, Measurements,
@@ -6,7 +18,13 @@ use tokio::{sync::mpsc, task};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let ui_handle = tokio::spawn(async {
+        spawn_ui_thread()
+            .await
+            .expect("Issue in spawning UI in main")
+    });
     fetch_all_data().await?;
+    ui_handle.await?;
 
     Ok(())
 }
@@ -75,4 +93,47 @@ async fn fetch_all_data() -> Result<(), Box<dyn std::error::Error>> {
     }
 
     Ok(())
+}
+
+async fn spawn_ui_thread() -> Result<(), Box<dyn std::error::Error>> {
+    let ui_handle = tokio::spawn(async {
+        create_ui().await.expect("Issue in opening TUI");
+    });
+    ui_handle.await?;
+
+    Ok(())
+}
+
+async fn create_ui() -> io::Result<()> {
+    enable_raw_mode()?;
+    stdout().execute(EnterAlternateScreen)?;
+    let mut terminal = Terminal::new(CrosstermBackend::new(stdout()))?;
+
+    let mut should_quit = false;
+    while !should_quit {
+        terminal.draw(ui)?;
+        should_quit = handle_events().await?;
+    }
+
+    disable_raw_mode()?;
+    stdout().execute(LeaveAlternateScreen)?;
+    Ok(())
+}
+
+async fn handle_events() -> io::Result<bool> {
+    if event::poll(std::time::Duration::from_millis(50))? {
+        if let Event::Key(key) = event::read()? {
+            if key.kind == event::KeyEventKind::Press && key.code == KeyCode::Char('q') {
+                return Ok(true);
+            }
+        }
+    }
+    Ok(false)
+}
+
+fn ui(frame: &mut Frame) {
+    frame.render_widget(
+        Paragraph::new("Hello World!").block(Block::bordered().title("Greeting")),
+        frame.area(),
+    );
 }
